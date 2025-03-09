6f1beb780ae483ad16ff4c7a4212e7a7571a572a
Raj Gupta <rajgupta.ckt22@gmail.com>
2025-03-09 22:41:01 +0530

    Added POTD for 09 March 2025

---
+++ March 2025/09 March 2025.cpp
@@ -0,0 +1,44 @@
+/*
+Palindrome SubStrings
+Given a string s, count all palindromic sub-strings present in the string. The length of the palindromic sub-string must be greater than or equal to 2.
+*/
+class Solution {
+    public:
+      int countPS(string &s) {
+          // code here
+           int n = s.length();
+      int res = 0;
+
+      vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+      // One length string is always palindrome
+      for (int i = 0; i < n; i++) {
+          dp[i][i] = true;
+      }
+
+      // Two length string is plaindrome if
+      // both characters are same
+      for (int i = 0; i < n - 1; i++) {
+          if (s[i] == s[i + 1]) {
+              dp[i][i + 1] = true;
+              res++;
+          }
+      }
+
+      // Handle palindromes of length
+      // greater than 2 (gap >= 2)
+      for (int gap = 2; gap < n; gap++) {
+          for (int i = 0; i < n - gap; i++) {
+              int j = i + gap;
+
+              // Check if the current string is a palindrome
+              if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                  dp[i][j] = true;
+                  res++;
+              }
+          }
+      }
+
+      return res;
+      }
+  };
