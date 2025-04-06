fc0250090b98a712453f7968e4d74855fc38a78e
Raj Gupta <rajgupta.ckt22@gmail.com>
2025-04-06 18:15:31 +0530

    Added POTD of 06 April 2025

---
+++ April 2025/06 April 2025.cpp
@@ -0,0 +1,38 @@
+//Topological sort
+class Solution {
+    public:
+      vector<int> topoSort(int V, vector<vector<int>>& edges) {
+          // code here
+          vector<vector<int>> adj(V);
+          vector<int> indegree(V,0);
+          for(auto e:edges){
+              adj[e[0]].push_back(e[1]);
+              indegree[e[1]]++;
+          }
+          vector<int> result;
+          queue<int> q;
+          for (int i = 0; i < V; i++) {
+              if (indegree[i] == 0) {
+                  q.push(i);
+              }
+          }
+          while (!q.empty()) {
+              int node = q.front();
+              q.pop();
+              result.push_back(node);
+
+              for (auto it : adj[node]) {
+                  indegree[it]--;
+                  if (indegree[it] == 0)
+                      q.push(it);
+              }
+          }
+
+          if (result.size() != V) {
+              cout << "Graph contains cycle!" << endl;
+              return {};
+          }
+
+          return result;
+      }
+  };
