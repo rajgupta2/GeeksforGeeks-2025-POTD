b47b14f6844b7d11e5b6376a1f031a21f4a807d9
Raj Gupta <rajgupta.ckt22@gmail.com>
2025-04-08 21:46:45 +0530

    Added POTD of 08 April 2025

---
+++ April 2025/08 April 2025.cpp
@@ -0,0 +1,14 @@
+//Minimum Number of Operations to Make Elements in Array Distinct
+class Solution {
+    public:
+        int minimumOperations(vector<int>& nums) {
+            vector<bool> seen(128);
+            for (int i = nums.size() - 1; i >= 0; i--) {
+                if (seen[nums[i]]) {
+                    return i / 3 + 1;
+                }
+                seen[nums[i]] = true;
+            }
+            return 0;
+        }
+    };
